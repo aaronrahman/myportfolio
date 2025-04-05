75fa3d322041c7800e8e84b5626f9b34cb41b363
Aaron Rahman <arahm32@uwo.ca>
2025-04-04 22:55:52 -0400

    adding a project

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useRef } from "react";
+import { useState} from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import { ChevronLeft, ChevronRight } from "lucide-react";
